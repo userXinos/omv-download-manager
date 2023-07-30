3726b12cea16dbdfc8ce2ee20dc1d29c8cc7d689
Xinos <userxinos@gmail.com>
2023-07-31 01:46:06 +0700

    lint

--- src/common/deleteAllCookies.ts
+++ src/common/deleteAllCookies.ts
@@ -1,10 +1,10 @@
 export function deleteAllCookies() {
-    const cookies = document.cookie.split(';');
+  const cookies = document.cookie.split(";");
 
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf('=');
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
-    }
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+  }
 }
